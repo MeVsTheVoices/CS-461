7b78075adf4108c64184a1cf2605557362910a47
Joshua Dunne <mevsthevoices@gmail.com>
2023-03-14 23:23:04 -0700

    finished project 4

--- Project 4/Project 4.lisp
+++ Project 4/Project 4.lisp
@@ -42,32 +42,73 @@
     (delete-if (lambda (x)
             (> (+ (- (car node) (car x)) (- (cadr node) (cadr x))) 2))
             all_nodes)
+    ;too many cannibals
+    (delete-if (lambda (x)
+            (> (cadr node) (car node)))
+            all_nodes)
     all_nodes
 )
 
+;run the given state against a set of predicates
+;will only return true for sensible states
 (defun is_valid_state (node) 
-    (cond 
+    (cond  
+        ;boat is either on the left or the right
         ((not (or (equal (caddr node) #\l) (equal (caddr node) #\r))) 
             nil)
+        ;we have between 3 and 0 missionaries on the left side
         ((or (< (car node) 0) (> (car node) 3))
             nil)
+        ;we have between 3 and 0 cannibals on the left side
         ((or (< (cadr node) 0) (> (cadr node) 3))
             nil)
         (t t)
     )
 )
 
-;all that changes based on the boats' side is that the
-;opposite side is calculated, then those values are used
-;to calculate the state
-(defun do_actions (node)
-    (if (is_valid_state node)
-        (case (caddr node) 
-            ((#\l)
-                (move_all_and_check node ()))
-            ((#\r)
-                (move_all_and_check (find_opposite_bank node) ()))
-        )
+;swaps the side of the boat in the list of states given
+(defun swap_sides_list (states)
+    ;apply swap_sides to the third element of every element
+    (mapcar (lambda (x)
+                `(,(car x) ,(cadr x) ,(swap_sides (caddr x)))
+            )
+        states
     )
 )
 
+;check for a valid state and then generate all possible
+;child states according to the missionaries and
+;cannibals problem
+(defun do_actions (node)
+    (if (is_valid_state node)
+        (case (caddr node) 
+            ;in both cases we move the boat to the other side
+            ;in all the states that were generated
+            ((#\l)
+                ;remove the initial point
+                (remove-if (lambda (x)
+                            (and (equal (car x) (car node))
+                                 (equal (cadr x) (cadr node))))
+                    ;calculate and swap the boats side
+                    (swap_sides_list (move_all_and_check node ()))))
+            ((#\r)
+                ;we use inverted values
+                (let ((swapped (find_opposite_bank node)))
+                    ;remove the initial point having calculated
+                    ;for the other side
+                    (remove-if (lambda (x)
+                            (and (equal (car x) (car swapped))
+                                 (equal (cadr x) (cadr swapped)))) 
+                    ;calculate from the inverted number and flip the boat
+                    (swap_sides_list (move_all_and_check (find_opposite_bank node) ())))))
+        )
+        ;if the state provided wasn't valid, print out and error
+        ;and take no further action
+        (format t "~S is not a valid state~%" node)
+    )
+)
+
+;passthrough to conform to the specifications
+(defun mac-next (node)
+    (do_actions node)
+)
