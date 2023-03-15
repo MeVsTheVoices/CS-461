a50c197b2c2f098d4f617f3264abd844b85f9b0d
Joshua Dunne <mevsthevoices@gmail.com>
2023-03-14 20:11:58 -0700

    almost finished with project 4

--- Project 4/Project 4.lisp
+++ Project 4/Project 4.lisp
@@ -1,34 +1,57 @@
+
+;utility function
+;assumes 3 cannibals and 3 missionaries
+;returns a state with the number of each on the opposite bank
+;and the side of the boat left unchanged
 (defun find_opposite_bank (list)
     (list (- 3 (car list)) (- 3 (cadr list)) (caddr list))
 )
 
-(defun move_left_to_right_accumulator(node accumulator)
-    (cond 
-        ((> (car node) 0)
-            (move_left_to_right_accumulator
-                `(,(- (car node) 1) ,(cadr node) ,(caddr node))
-                (cons node accumulator)
-            ))
-        ((> (cadr node) 0)
-            (move_left_to_right_accumulator
-                `(,(car node) ,(- (cadr node) 1) ,(caddr node))
-                (cons node accumulator)
-            ))
-        (t
-            accumulator)
+;utility function
+;returns the same state with the boat
+;on the opposite side
+(defun swap_sides (side) 
+    (cond
+        ((equal side #\r) #\l) 
+        ((equal side #\l) #\r)
+        (t side)
     )
 )
 
-(defun move_left_to_right (node)
-    (move_left_to_right_accumulator node ())
+;generate every possible permuation by subtracting
+;all combinations of 0, 1, 2 missionaries and
+;0, 1, 2 cannibals
+(defun move_all (node)
+    (loop for a from 0 to 2 collect
+        (let ((i_node `(,(- (car node) a) ,(cadr node) ,(caddr node))))
+            (loop for b from 0 to 2 collect
+                `(,(car i_node) ,(- (cadr i_node) b) ,(caddr i_node))))
+    )
 )
 
+;apply some prediates to every possible permuation
+;to maintain constistency with the problem
+(defun move_all_and_check (node accumulator)
+    ;remove one level of nesting
+    (setq all_nodes (mapcan (lambda (x) x) (move_all node)))
+    (delete-if (lambda (x) 
+            (or (< (car x) 0)
+                (< (cadr x) 0))) all_nodes)
+    (delete-if (lambda (x)
+            (> (+ (- (car node) (car x)) (- (cadr node) (cadr x))) 2))
+            all_nodes)
+    all_nodes
+)
+
+;all that changes based on the boat is that the
+;opposite side is calculated, then those values used
+;to calculate the state
 (defun do_actions (node)
     (if (equal (caddr node) #\l)
-        (princ (move_left_to_right node))
+        (princ (remove-duplicates (move_left_to_right node)))
     )
     (if (equal (caddr node) #\r)
-        (princ (move_left_to_right (find_opposite_bank node)))
+        (princ (remove-duplicates (move_left_to_right (find_opposite_bank node))))
     )
 )
 
