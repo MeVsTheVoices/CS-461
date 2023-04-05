57111a3f65f23fa988405e3a088c2737be792eb9
Joshua Dunne <mevsthevoices@gmail.com>
2023-04-04 22:13:37 -0700

    fixed behavoir, squared away junk code

--- Project 5/Project 5.lisp
+++ Project 5/Project 5.lisp
@@ -1,3 +1,9 @@
+(defun swap-side (node)
+    (case (caddr node) 
+        (#\l #\r)
+        (#\r #\l) )
+)
+
 (defun di(m n)
     (cond
         ((null n) nil)
@@ -26,24 +32,26 @@
 
 (defun illegal-transition (a b)
     (cond 
-        ((> b a) t)
-        ((< (+ a b) 1) t)
-        ((> (+ a b) 2) t)
-        (t nil)
+        ((and (equal a 1) (equal b 1)) nil)
+        ((and (equal a 0) (equal b 1)) nil)
+        ((and (equal a 1) (equal b 0)) nil)
+        (t t)
     )
 )
 
 (defun find-transition (was is)
-    `(,(- (car was) (car is)) ,(- (cadr was) (cadr is)))
+    `(,(abs (- (car was) (car is))) ,(abs (- (cadr was) (cadr is))))
 )
 
 (defun if-is-illegal-transition (was is)
     (let ((transition (find-transition was is)))
         (cond
-            ((illegal-transition (car transition) (cadr transition))
-                t)
-            ((equal (caddr was) (caddr is))
-                t)
+            ((and (equal (car transition) 1) (equal (cadr transition) 1)) nil)
+            ((and (equal (car transition) 0) (equal (cadr transition) 1)) nil)
+            ((and (equal (car transition) 1) (equal (cadr transition) 0)) nil)
+            ((and (equal (car transition) 2) (equal (cadr transition) 0)) nil)
+            ((and (equal (car transition) 0) (equal (cadr transition) 2)) nil)
+            (t t)
         )
     )
 )
@@ -54,7 +62,11 @@
             (cannibals (cadr is))
             )
         (cond
-            ((> cannibals missionaries) t)
+            ((> cannibals missionaries)
+                (cond
+                    ((equal missionaries 0) nil)
+                    (t t)
+                ))
             ((or (> cannibals 3) (> missionaries 3)) t)
             ((or (< cannibals 0) (< missionaries 0)) t)
             (t nil)
@@ -65,10 +77,44 @@
     (map 'list (lambda (x) (append x (cons tag nil))) (cartesian-ranges (car node) (cadr node)))
 )
 
-(defun generate-legal-cartesian-states (node tag)
-    (let ((generated-states (generate-cartesian-states node tag)))
-        (delete-if #'if-is-illegal-state generated-states)
-        (delete-if (lambda (x) (if-is-illegal-transition node x)) generated-states)
+(defun generate-legal-cartesian-states (node)
+    (if (equal (caddr node) #\r)
+        (setq generated-states
+                (generate-cartesian-states 
+                    `(,(- 3 (car node)) ,(- 3 (cadr node)) ,(caddr node)) (swap-side node)))
+        (setq generated-states
+                (generate-cartesian-states node (swap-side node)))
+    )
+    (print node)
+    (print generated-states)
+    (remove-if (lambda (x)
+        (let (
+            (inverse (list (- 3 (car x)) (- 3 (cadr x)) (caddr x)))
+            )
+            (cond
+                ((if-is-illegal-state x) t)
+                ((if-is-illegal-state inverse) t)
+                ((if-is-illegal-transition node x) t)
+                ((and (equal
+                        (car node) (car x))
+                      (equal
+                        (cadr node) (cadr x)) t))
+            )
+        )
+    ) generated-states)
+)
 
+(defun keep-trying (node goal)
+    (let ((generated-states (generate-legal-cartesian-states node)))
+        (print node)
+        (print generated-states)
+        (cond
+            ((null generated-states) nil)
+            ((or
+                (member `(,(car goal) ,(cadr goal), #\l) generated-states)
+                (member `(,(car goal) ,(cadr goal), #\l) generated-states))
+                goal)
+            (t (keep-trying (car generated-states) goal))
+        )
     )
 )
